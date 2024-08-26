bc70e23eb3c4750c900ae7ed3080c158547672d4
Jagrit Kr Jain <141074124+Jagrit-Kr-Jain@users.noreply.github.com>
2024-08-27 00:49:30 +0530

    Update SQL_Database_edoc.sql

--- SQL_Database_edoc.sql
+++ SQL_Database_edoc.sql
@@ -1,29 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 19, 2022 at 01:39 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.5
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `edoc`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `admin`
 --
@@ -42,8 +20,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 INSERT INTO `admin` (`aemail`, `apassword`) VALUES
 ('admin@edoc.com', '123');
 
--- --------------------------------------------------------
-
+-- -------------------------------------------------------
 --
 -- Table structure for table `appointment`
 --
